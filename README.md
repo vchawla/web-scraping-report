6c4bae4f46fa3c29a9a4759da3cfc5f133986c90
vchawla <vishal.k.chawla@gmail.com>
2022-09-05 01:45:37 +0800

    Section 2

--- README.md
+++ README.md
@@ -13,7 +13,8 @@ The data is extracted from Firebase Firestore which is saved by scripts that are
 
 ## Section 2
 
-TBD
+Section 2 shows difference in sales, buyers and transactions by Projects.<br/>
+Currently data is collected for only 3 projects - Immutaswap.io, Illuvium, Gods Unchained
 
 ## Future enhancements
 
@@ -21,6 +22,20 @@ TBD
 1.] Add comma in the numbers in the leaderboard table for better readability. Currently comma is skipped because of table sort functionality <br />
 2.] The UI can be enhanced with more elegant templates <br/>
 3.] Powerful visualizations can be implemented by using [D3.js](https://d3js.org/)
+4.] Data fetch timestamp to be made dynamic
+5.] Security aspects to be reviewed and addressed if needed
 
 ### Section 2
-TBD
+1.] Currently only 3 project data is available. But the scraper script can be run for any number of projects as needed
+2.] Numbers in tables can be properly formatted
+
+
+## Disclaimer
+
+These crawlers have been implemented for assignment/education purposes. Legal teams of respective websites should be consulted with for any production implementation
+
+### References
+
+[https://www.youtube.com/playlist?list=PL4cUxeGkcC9jERUGvbudErNCeSZHWUVlb](https://www.youtube.com/playlist?list=PL4cUxeGkcC9jERUGvbudErNCeSZHWUVlb) <br/>
+[https://www.youtube.com/watch?v=w7xKZ5PWizs](https://www.youtube.com/watch?v=w7xKZ5PWizs) <br/>
+[http://tabulator.info/](http://tabulator.info/)
--- src/index.js
+++ src/index.js
@@ -85,4 +85,218 @@ getDocs(CSColRef)
   })
 
 
+ // collection ref Projects
+var CSColRef = collection(db, 'Projects')
+
+var chains = [];
+var chain_data = [];
+
+var sales_1d = [];
+var sales_7d = [];
+var sales_30d = [];
+
+var buyers_1d = [];
+var buyers_7d = [];
+var buyers_30d = [];
+
+
+var txns_1d = [];
+var txns_7d = [];
+var txns_30d = [];
+
+
+// get collection data
+getDocs(CSColRef)
+  .then(snapshot => {
+    
+    snapshot.docs.forEach(doc => {
+        // chains.push({ ...doc.data(), id: doc.id })
+        var curr_data = doc.data();
+        console.log(curr_data);
+
+        var buyers_1d_row={};
+        buyers_1d_row["project"]= doc.id;
+        buyers_1d_row["cs"]= curr_data["cs_total_buyers_last_day"];
+        buyers_1d_row["im"]= curr_data["im_total_buyers_last_day"];
+        buyers_1d_row["diff"]= curr_data["im_total_buyers_last_day"] - curr_data["cs_total_buyers_last_day"];
+        buyers_1d.push(buyers_1d_row);
+
+        var buyers_7d_row={};
+        buyers_7d_row["project"]= doc.id;
+        buyers_7d_row["cs"]= curr_data["cs_total_buyers_last_7day"];
+        buyers_7d_row["im"]= curr_data["im_total_buyers_last_7day"];
+        buyers_7d_row["diff"]= curr_data["im_total_buyers_last_7day"] - curr_data["cs_total_buyers_last_7day"];
+        buyers_7d.push(buyers_7d_row);
+
+        var buyers_30d_row={};
+        buyers_30d_row["project"]= doc.id;
+        buyers_30d_row["cs"]= curr_data["cs_total_buyers_last_30day"];
+        buyers_30d_row["im"]= curr_data["im_total_buyers_last_30day"];
+        buyers_30d_row["diff"]= curr_data["im_total_buyers_last_30day"] - curr_data["cs_total_buyers_last_30day"];
+        buyers_30d.push(buyers_30d_row);
+
+        var sales_1d_row={};
+        sales_1d_row["project"]= doc.id;
+        sales_1d_row["cs"]= curr_data["cs_total_sales_last_day"];
+        sales_1d_row["im"]= curr_data["im_total_sales_last_day"];
+        sales_1d_row["diff"]= curr_data["im_total_sales_last_day"] - curr_data["cs_total_sales_last_day"];
+        sales_1d.push(sales_1d_row);
+
+        var sales_7d_row={};
+        sales_7d_row["project"]= doc.id;
+        sales_7d_row["cs"]= curr_data["cs_total_sales_last_7day"];
+        sales_7d_row["im"]= curr_data["im_total_sales_last_7day"];
+        sales_7d_row["diff"]= curr_data["im_total_sales_last_7day"] - curr_data["cs_total_sales_last_7day"];
+        sales_7d.push(sales_7d_row);
+
+        var sales_30d_row={};
+        sales_30d_row["project"]= doc.id;
+        sales_30d_row["cs"]= curr_data["cs_total_sales_last_30day"];
+        sales_30d_row["im"]= curr_data["im_total_sales_last_30day"];
+        sales_30d_row["diff"]= curr_data["im_total_sales_last_30day"] - curr_data["cs_total_sales_last_30day"];
+        sales_30d.push(sales_30d_row);
+
+
+        var txns_1d_row={};
+        txns_1d_row["project"]= doc.id;
+        txns_1d_row["cs"]= curr_data["cs_total_transactions_last_day"];
+        txns_1d_row["im"]= curr_data["im_total_transactions_last_day"];
+        txns_1d_row["diff"]= curr_data["im_total_transactions_last_day"] - curr_data["cs_total_transactions_last_day"];
+        txns_1d.push(txns_1d_row);
+
+        var txns_7d_row={};
+        txns_7d_row["project"]= doc.id;
+        txns_7d_row["cs"]= curr_data["cs_total_transactions_last_7day"];
+        txns_7d_row["im"]= curr_data["im_total_transactions_last_7day"];
+        txns_7d_row["diff"]= curr_data["im_total_transactions_last_7day"] - curr_data["cs_total_transactions_last_7day"];
+        txns_7d.push(txns_7d_row);
+
+        var txns_30d_row={};
+        txns_30d_row["project"]= doc.id;
+        txns_30d_row["cs"]= curr_data["cs_total_transactions_last_30day"];
+        txns_30d_row["im"]= curr_data["im_total_transactions_last_30day"];
+        txns_30d_row["diff"]= curr_data["im_total_transactions_last_30day"] - curr_data["cs_total_transactions_last_30day"];
+        txns_30d.push(txns_30d_row);
+      
+        var chain_table = new Tabulator("#sales-table-1d", {
+            height:"130px", // set height of table
+            data:sales_1d, //assign data to table
+            layout:"fitColumns", //fit columns to width of table (optional)
+            columns:[ //Define Table Columns
+                {title:"Project", field:"project", width:250},
+                {title:"IM Sales", field:"im", width:250},
+                {title:"CS Sales", field:"cs", width:250},
+                {title:"Difference", field:"diff", width:250}
+            ],
+        });
+
+        var chain_table = new Tabulator("#sales-table-7d", {
+          height:"130px", // set height of table
+          data:sales_7d, //assign data to table
+          layout:"fitColumns", //fit columns to width of table (optional)
+          columns:[ //Define Table Columns
+              {title:"Project", field:"project", width:250},
+              {title:"IM Sales", field:"im", width:250},
+              {title:"CS Sales", field:"cs", width:250},
+              {title:"Difference", field:"diff", width:250}
+          ],
+      });        
+
+
+      var chain_table = new Tabulator("#sales-table-30d", {
+        height:"130px", // set height of table
+        data:sales_30d, //assign data to table
+        layout:"fitColumns", //fit columns to width of table (optional)
+        columns:[ //Define Table Columns
+            {title:"Project", field:"project", width:250},
+            {title:"IM Sales", field:"im", width:250},
+            {title:"CS Sales", field:"cs", width:250},
+            {title:"Difference", field:"diff", width:250}
+        ],
+    });
+
+            var chain_table = new Tabulator("#buyers-table-1d", {
+            height:"130px", // set height of table
+            data:buyers_1d, //assign data to table
+            layout:"fitColumns", //fit columns to width of table (optional)
+            columns:[ //Define Table Columns
+                {title:"Project", field:"project", width:250},
+                {title:"IM Buyers", field:"im", width:250},
+                {title:"CS Buyers", field:"cs", width:250},
+                {title:"Difference", field:"diff", width:250}
+            ],
+        });
+
+        var chain_table = new Tabulator("#buyers-table-7d", {
+          height:"130px", // set height of table
+          data:buyers_7d, //assign data to table
+          layout:"fitColumns", //fit columns to width of table (optional)
+          columns:[ //Define Table Columns
+              {title:"Project", field:"project", width:250},
+              {title:"IM Buyers", field:"im", width:250},
+              {title:"CS Buyers", field:"cs", width:250},
+              {title:"Difference", field:"diff", width:250}
+          ],
+      });        
+
+
+      var chain_table = new Tabulator("#buyers-table-30d", {
+        height:"130px", // set height of table
+        data:buyers_30d, //assign data to table
+        layout:"fitColumns", //fit columns to width of table (optional)
+        columns:[ //Define Table Columns
+            {title:"Project", field:"project", width:250},
+            {title:"IM Buyers", field:"im", width:250},
+            {title:"CS Buyers", field:"cs", width:250},
+            {title:"Difference", field:"diff", width:250}
+        ],
+    });
+
+            var chain_table = new Tabulator("#txns-table-1d", {
+            height:"130px", // set height of table
+            data:txns_1d, //assign data to table
+            layout:"fitColumns", //fit columns to width of table (optional)
+            columns:[ //Define Table Columns
+                {title:"Project", field:"project", width:250},
+                {title:"IM Txns", field:"im", width:250},
+                {title:"CS Txns", field:"cs", width:250},
+                {title:"Difference", field:"diff", width:250}
+            ],
+        });
+
+        var chain_table = new Tabulator("#txns-table-7d", {
+          height:"130px", // set height of table
+          data:txns_7d, //assign data to table
+          layout:"fitColumns", //fit columns to width of table (optional)
+          columns:[ //Define Table Columns
+              {title:"Project", field:"project", width:250},
+              {title:"IM Txns", field:"im", width:250},
+              {title:"CS Txns", field:"cs", width:250},
+              {title:"Difference", field:"diff", width:250}
+          ],
+      });        
+
+
+      var chain_table = new Tabulator("#txns-table-30d", {
+        height:"130px", // set height of table
+        data:txns_30d, //assign data to table
+        layout:"fitColumns", //fit columns to width of table (optional)
+        columns:[ //Define Table Columns
+            {title:"Project", field:"project", width:250},
+            {title:"IM Txns", field:"im", width:250},
+            {title:"CS Txns", field:"cs", width:250},
+            {title:"Difference", field:"diff", width:250}
+        ],
+    });
+
+
+    })
+    
+  }
+  )
+  .catch(err => {
+    console.log(err.message)
+  }) 
+
+
  
